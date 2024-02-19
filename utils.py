faeb92d9bd73036d37f2c5ce32b11e407c11d562
Yurii Miniailyk <uragenesis206@gmail.com>
2024-02-19 19:07:38 +0200

    Added prime(n) function

--- utils.py
+++ utils.py
@@ -3,3 +3,10 @@ def fact(n):
         return 1
     else:
         return n * fact(n-1)
+
+def prime(n):
+    ans=True
+    for i in range(2,int(math.sqrt(n))+1):
+        if n%i==0:
+            ans=False
+    return ans
